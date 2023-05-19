2c7713aa4cc066f1de6fc3e2105702ea22362c0f
sobiaahmad94 <sobiaahmad94@icloud.com>
2023-05-19 01:00:58 +0100

    created some of the components

---
+++ src/components/DigimonContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const DigimonContainer = () => {
+    return (
+        <>
+            <h1>This is the DigimonContainer</h1>
+
+        </>
+    )
+}
+
+export default DigimonContainer
